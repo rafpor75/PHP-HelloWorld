4f435059339737d9906edf050781a0ac35ca28ed
Raffaele Porzio <37336855+rafpor75@users.noreply.github.com>
2019-03-19 14:01:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-Happy Learning. This is my first php page</p>'; ?> 
+ <?php echo '<p>Hello World Happy Learning. This is my updated php page</p>'; ?> 
  </body>
 </html>
